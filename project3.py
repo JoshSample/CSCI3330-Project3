fae621d81b6338bf1268ad618e5bc8e2517a30ed
JoshSample <joshsample98@gmail.com>
2019-10-31 13:10:45 -0500

    Problem 2 complete

--- project3.py
+++ project3.py
@@ -8,13 +8,35 @@ import networkx as nx  # This is the graph ADT we are working with
 
 def dfs_paths(graph, start, goal):
     stack = [(start, [start])]
+    visited = set()
     while stack:
         (vertex, path) = stack.pop()
-        for next in graph[vertex] - set(path):
-            if next == goal:
-                yield path + [next]
-            else:
-                stack.append((next, path + [next]))
+        if vertex not in visited:
+            if vertex == goal:
+                return path
+            visited.add(vertex)
+            for neighbor in graph[vertex]:
+                stack.append((neighbor, path + [neighbor]))
+
+
+def bfs_path(graph, start, end):
+    # maintain a queue of paths
+    queue = []
+    # push the first path into the queue
+    queue.append([start])
+    while queue:
+        # get the first path from the queue
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        # path found
+        if node == end:
+            return path
+        # enumerate all adjacent nodes, construct a new path and push it into the queue
+        for adjacent in graph.get(node, []):
+            new_path = list(path)
+            new_path.append(adjacent)
+            queue.append(new_path)
 
 
 def problem_1(graph):
@@ -26,15 +48,15 @@ def problem_1(graph):
             G1.add_edge(k, vv)
 
     # Problem 1
-    print("DFS:", list(nx.dfs_edges(G1, "a")))
-    print("DFS with other remaining edges:", list(nx.dfs_edges(G1, "h")))
-    print("BFS:", list(nx.bfs_edges(G1, "a")))
-    print("BFS with other remaining edges:", list(nx.bfs_edges(G1, "h")))
+    print("DFS:", list(nx.dfs_edges(G1, 'a')))
+    print("DFS with other remaining edges:", list(nx.dfs_edges(G1, 'h')))
+    print("BFS:", list(nx.bfs_edges(G1, 'a')))
+    print("BFS with other remaining edges:", list(nx.bfs_edges(G1, 'h')))
 
 
 def main():
     # Define the first graph from problem 1
-    graph1 = {'a': set(['b', 'f', 'e']),
+    graph1 = {'a': set(['b', 'e', 'f']),
               'b': set(['a', 'c', 'f']),
               'c': set(['b', 'd', 'g']),
               'd': set(['c', 'g']),
@@ -53,8 +75,10 @@ def main():
     # Do problem 1
     problem_1(graph1)
     # Problem 2
-    v = dfs_paths(graph1, 'a', 'c')
-    print("DFS path: ", list(v))
+    v = dfs_paths(graph1, 'h', 'p')
+    print("DFS path from a to c: ", list(v))
+    w = bfs_path(graph1, 'h', 'p')
+    print("BFS path from a to c: ", list(w))
     # Problem 3
     G2 = nx.Graph()
     G2.add_edges_from([(1, 3), (3, 5), (3, 2), (2, 1), (4, 1),
